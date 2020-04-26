2d1330750fd8f14aa6986c222a8a20152c698b28
dunkelmoses <moses.hashim@gmail.com>
2020-04-26 09:58:07 -0400

    added: list in the settings

---
+++ app/src/main/java/com/example/apeschat/MyOwnAdapter.java
@@ -0,0 +1,10 @@
+package com.example.apeschat;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+
--- app/src/main/java/com/example/apeschat/Settings.java
+++ app/src/main/java/com/example/apeschat/Settings.java
@@ -3,16 +3,36 @@ package com.example.apeschat;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Set;
 
 public class Settings extends AppCompatActivity {
-
+    private ListView listView;
+//    private String [] settingList =
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        listView = findViewById(R.id.listItems);
+        MyOwnAdapter adapter = new MyOwnAdapter();
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener((arg0, arg1, position, arg3) -> {
+
+        });
+
 
         Toolbar toolbar = findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
@@ -20,9 +40,52 @@ public class Settings extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
             }
         });
+    }
 
+    public class MyOwnAdapter extends BaseAdapter {
+        String[] data = new String[]{"Change Password","Change Username"};
+
+        @Override
+        public int getCount() {
+            return data.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return data[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater inflater = getLayoutInflater();
+            convertView = inflater.inflate(R.layout.rowsettings, parent, false);
+            TextView oneRow = convertView.findViewById(R.id.itemSettingList);
+            ImageView arow = convertView.findViewById(R.id.arow);
+            oneRow.setText(data[position]);
+            arow.setImageResource(R.drawable.arow);
+            oneRow.setOnClickListener(e->{
+                if (position==0){
+                    Toast.makeText(Settings.this,"this is 0"+position,Toast.LENGTH_LONG).show();
+                }
+                else if (position==1){
+                    Toast.makeText(Settings.this,"this is 1"+position,Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(Settings.this,"idk"+position,Toast.LENGTH_LONG).show();
+
+                }
+//                startActivity(new Intent(Settings.this,MainAppPage.class));
+            });
+            return convertView;
+        }
     }
 }
